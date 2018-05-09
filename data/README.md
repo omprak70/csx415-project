cb377935dc86539c7f77b1e0941ab06900ae4831
omprak70 <omprak@gmail.com>
2018-05-09 14:52:09 -0700

    Updated

---
+++ data/README.md
@@ -0,0 +1,18 @@
+Flow imbalance test data from VorTeq missile field test is included here
+
+Data combined from various test cases
+- In half the test cases, process flow is fixed
+- In the other half of the test cases, motive flow is fixed
+- In each half, there are test cases where
+   - all RPXs are running
+   - 11 RPXs are running
+   - 10 RPXs are running
+   - 8 RPXs are running
+   - 7 RPXs are running
+   - 6 RPXs are running
+
+Features include (30 total)
+- Process and Motive flows for each of the 12 RPXs (24 columns)
+- Average Process & Motive Flows as well as Standard Deviations (4 columns)
+- Number of PXs running
+- Erosion potential
---
+++ src/Naive_Model.R
@@ -0,0 +1,23 @@
+setwd("C:/users/ompra/documents/UCBcourses/Datascience/csx415-Project")
+
+library('ProjectTemplate')
+load.project()
+
+# Read Data
+TData <- FlowImbalanceTestData.LP.HP.p125
+
+#Shuffle Data
+SData <- TData[sample(nrow(TData)),]
+
+#Split Shuffled Data into training data, validation data & test data
+TrgData <- SData[1:(0.6*nrow(SData)),]
+ValData <- SData[(0.6*nrow(SData)):(0.8*nrow(SData)),]
+TstData <- SData[(0.8*nrow(SData)):nrow(SData),]
+
+#Build histogram of response variable - Erosion Potential
+ggplot(data=TrgData,aes(x=ErosionPotential)) +
+  geom_histogram(binwidth=0.02,fill="Magenta",color="Black")
+
+#Build a Naive Model to predict Erosion Potential
+#Take the mean
+ErPot_Naive = mean(TrgData$ErosionPotential)
